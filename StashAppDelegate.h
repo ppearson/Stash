6754a39c5b40cae223e391b146aafdd8f2be8b64
Peter Pearson <peter.pearson@gmail.com>
2009-11-22 14:15:32 +0000

    Add copyright and GPL 2 notice comments at the top of each file.

--- StashAppDelegate.h
+++ StashAppDelegate.h
@@ -1,10 +1,24 @@
-//
-//  StashAppDelegate.h
-//  Stash
-//
-//  Created by Peter Pearson on 24/10/2009.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
-//
+/* 
+ * Stash:  A Personal Finance app for OS X.
+ * Copyright (C) 2009 Peter Pearson
+ * You can view the complete license in the Licence.txt file in the root
+ * of the source tree.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
 
 #import <Cocoa/Cocoa.h>
 #include "account.h"
