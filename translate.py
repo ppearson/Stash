4cc08f90b4d77db79ffc8d0f9582e48fd79cf1ac
Peter Pearson <peter.pearson@gmail.com>
2010-02-07 18:53:23 +0000

    Add Python script to help handle creation of string files and .xibs for localisations.

---
+++ translate.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+# Translate utility for managing localisation strings with xib resource bundles for Stash
+# version 1.0
+
+import sys
+import os
+
+xibFiles = ["AccountInfo", "AddAccount", "DueScheduledTransactions", "ExportOFX", "ImportOFX", "ImportQIF", "MainMenu", "MakeTransfer", "Preferences"]
+
+def generateStringFiles(pathToCreateIn):
+	print "gen"
+	if pathToCreateIn[-1] != "/":
+		pathToCreateIn += "/"
+	
+	for file in xibFiles:
+		strCommand = "ibtool --export-strings-file %s%s.strings English.lproj/%s.xib" % (pathToCreateIn, file, file)
+#		print strCommand
+		os.system(strCommand)
+	
+	print "String files generated."
+
+def createXibFiles(pathOfStringFiles, newLocalisation):
+	if pathOfStringFiles[-1] != "/":
+		pathOfStringFiles += "/"
+		
+	strNewDir = "%s.lproj/" % (newLocalisation)
+	
+	try:
+		os.mkdir(strNewDir)
+	except os.error:
+		pass
+	
+	for file in xibFiles:
+		strCommand = "ibtool --strings-file %s%s.strings --write %s%s.xib English.lproj/%s.xib" % (pathOfStringFiles, file, strNewDir, file, file)
+#		print strCommand
+		os.system(strCommand)
+	
+	print ".xib files created."
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		print "Invalid parameters specified.\nCommands are:\ntranslate.py genstrings <pathForStringFiles>\ntranslate.py createxibs <pathOfStringFiles> <newLocalisationName>\n"
+		sys.exit(2)
+	
+	if sys.argv[1] == "genstrings":
+		pathToCreateIn = sys.argv[2]
+		if len(pathToCreateIn) > 0:
+			generateStringFiles(pathToCreateIn)
+	elif sys.argv[1] == "createxibs":
+		pathOfStringFiles = sys.argv[2]
+		newLocalisation = sys.argv[3]
+		createXibFiles(pathOfStringFiles, newLocalisation)
+
